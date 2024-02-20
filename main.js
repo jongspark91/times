74e410346f6281e86c99cd3a96f980dfd02a79bc
mapark <jongspark91@gmail.com>
2024-02-21 01:09:57 +0900

    20240221_2

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const apiKey = "8341e0d5704a413d903d3f1ca6ba4bc3"
 let newsList = []
+let newsDesc = ""
+let newsImg = ""
+let newsSrc = ""
 const getLatestNews = async ()=>{
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
-    const url = new URL(`https://mapark-times.netlify.app/top-headlines?country=kr`);
+    const url = new URL(`https://mapark-times.netlify.app/top-headlines?`);
     const response= await fetch(url);
     const data = await response.json();
     newsList = data.articles;
-    // console.log(newsList);
-    // render()
+    console.log(newsList);
+    render()
     // console.log("rrr",response);
 }
 
@@ -15,18 +18,47 @@ const render=()=>{
     const newsHTML = newsList.map(
         (news)=> `<div class = "row news">
     <div class = "col-lg-4">
-        <img class=${news.urlToImage}/>
+        <img class= "news-img-size" src = "${checkImage(news.urlToImage)}"/>
     </div>
     <div class = "col-lg-8">
         <h2>${news.title}</h2>
         <p>
-            ${news.description}
+            ${sliceDescription(news.description)}
         </p>
         <div>
-            ${news.source.name} * ${news.publishedAt}
+            ${checkSource(news.source.name)} * ${moment(news.publishedAt).fromNow()}
         </div>
     </div>
 </div>`).join('');
     document.getElementById("news-board").innerHTML = newsHTML
 }
 getLatestNews()
+
+function sliceDescription(desc){
+    if(desc.length >= 200){
+        newsDesc = desc.substr(0,200) + "..."
+    }else if(desc.length === 0){
+        newsDesc = "내용 없음"
+    }else{
+        newsDesc = desc
+    }
+    return newsDesc
+}
+
+function checkImage(img){
+    if(img===""){
+        newsImg = "이미지 없음"
+    }else{
+        newsImg = img
+    }
+    return newsImg
+}
+
+function checkSource(item){
+    if(item===""){
+        newsSrc = "출처 없음"
+    }else{
+        newsSrc = item
+    }
+    return newsSrc
+}
